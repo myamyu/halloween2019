f17b823b0ad818695cbca67a8d3f5c8ed2be847a
myamyu <myamyu@gmail.com>
2019-10-16 09:50:08 +0900

    カボチャ出現

--- src/index.js
+++ src/index.js
@@ -7,13 +7,49 @@ const svgDom = (str) => {
 };
 
 const halloween = () => {
+  require('./halloween.css');
+  const body = d.getElementsByTagName('body')[0];
   const div = d.createElement('div');
   div.style = 'display:none;';
-  d.getElementsByTagName('body')[0].appendChild(div);
+  body.appendChild(div);
 
   div.appendChild(svgDom(require('../assets/svg/ghost.svg')));
   div.appendChild(svgDom(require('../assets/svg/pumpkin.svg')));
   div.appendChild(svgDom(require('../assets/svg/pumpkin2.svg')));
+
+  // willが出現
+  body.appendChild(svgDom(`<svg xmlns="http://www.w3.org/2000/svg" version="1.1"
+    xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 320 320"
+    class="halloween-ghost-will">
+      <use xlink:href="#ghost" />
+    </svg>`));
+
+  // listの頭にカボチャ
+  const lis = d.getElementsByTagName('li');
+  const pumpkins = ['pumpkin', 'pumpkin2'];
+  Array.from(lis).forEach((li) => {
+    const pumpkinDom = svgDom(`<svg xmlns="http://www.w3.org/2000/svg" version="1.1"
+      xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 320 280"
+      class="halloween-pumpkin">
+        <use xlink:href="#${pumpkins[0|Math.random()*pumpkins.length]}" />
+      </svg>`);
+    li.insertBefore(pumpkinDom, li.firstChild);
+  });
+
+  // hタグの後ろにカボチャ
+  const hs = d.querySelectorAll('h1,h2,h3,h4,h5,h6');
+  Array.from(hs).forEach((h) => {
+    const pumpkinDom = svgDom(`<svg xmlns="http://www.w3.org/2000/svg" version="1.1"
+      xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 480 320"
+      class="halloween-pumpkins">
+        <use xlink:href="#${pumpkins[0|Math.random()*pumpkins.length]}"
+          x="0" y="0" />
+        <use xlink:href="#${pumpkins[0|Math.random()*pumpkins.length]}"
+          transform="scale(0.7)"
+          x="220" y="160" />
+      </svg>`);
+    h.appendChild(pumpkinDom);
+  });
 };
 
 (() => {
